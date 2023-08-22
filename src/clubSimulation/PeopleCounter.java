9d0db28ce80d470f166251b1d9532ca9023de00f
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2023-08-22 16:03:55 +0200

    Pause + start buttons working

--- src/clubSimulation/PeopleCounter.java
+++ src/clubSimulation/PeopleCounter.java
@@ -1,19 +1,20 @@
 package clubSimulation;
+
 import java.util.concurrent.atomic.*;
 
 public class PeopleCounter {
-	private AtomicInteger peopleOutSide; //counter for people arrived but not yet in the building
-	private AtomicInteger peopleInside; //counter for patrons inside club
-	private AtomicInteger peopleLeft; //counter for patrons who have left the club
-	private AtomicInteger maxPeople; //maximum patrons allowed in the club at one time
-	
+	private AtomicInteger peopleOutSide; // counter for people arrived but not yet in the building
+	private AtomicInteger peopleInside; // counter for patrons inside club
+	private AtomicInteger peopleLeft; // counter for patrons who have left the club
+	private AtomicInteger maxPeople; // maximum patrons allowed in the club at one time
+
 	PeopleCounter(int max) {
-		peopleOutSide= new AtomicInteger(0);
-		peopleInside=new AtomicInteger(0);
-		peopleLeft=new AtomicInteger(0);
-		maxPeople=new AtomicInteger(max);
+		peopleOutSide = new AtomicInteger(0);
+		peopleInside = new AtomicInteger(0);
+		peopleLeft = new AtomicInteger(0);
+		maxPeople = new AtomicInteger(max);
 	}
-		
+
 	public int getWaiting() {
 		return peopleOutSide.get();
 	}
@@ -21,44 +22,45 @@ public class PeopleCounter {
 	public int getInside() {
 		return peopleInside.get();
 	}
-	
+
 	public int getTotal() {
-		return (peopleOutSide.get()+peopleInside.get()+peopleLeft.get());
+		return (peopleOutSide.get() + peopleInside.get() + peopleLeft.get());
 	}
 
 	public int getLeft() {
 		return peopleLeft.get();
 	}
-	
+
 	public int getMax() {
 		return maxPeople.get();
 	}
-	
-	//someone arrived outside
+
+	// someone arrived outside
 	public void personArrived() {
 		peopleOutSide.getAndIncrement();
 	}
-	
-	//someone got inside
+
+	// someone got inside
 	synchronized public void personEntered() {
 		peopleOutSide.getAndDecrement();
 		peopleInside.getAndIncrement();
 	}
 
-	//someone left
+	// someone left
 	synchronized public void personLeft() {
 		peopleInside.getAndDecrement();
 		peopleLeft.getAndIncrement();
-		
+
 	}
-	//too many people inside
+
+	// too many people inside
 	synchronized public boolean overCapacity() {
-		if(peopleInside.get()>=maxPeople.get())
+		if (peopleInside.get() >= maxPeople.get())
 			return true;
 		return false;
 	}
-	
-	//not used
+
+	// not used
 	synchronized public void resetScore() {
 		peopleInside.set(0);
 		peopleOutSide.set(0);
--- src/clubSimulation/PeopleLocation.java
+++ src/clubSimulation/PeopleLocation.java
@@ -4,58 +4,73 @@ import java.awt.Color;
 import java.util.Random;
 import java.util.concurrent.atomic.*;
 
-public class PeopleLocation  { // this is a separate class so don't have to access thread
-	
-	private final int ID; //each person has an ID
-	private Color myColor; //colour of the person
-	private AtomicBoolean inRoom; //are they in the club?
-	private AtomicBoolean arrived; //have they arrived at the club?
-	private GridBlock location; //which GridBlock are they on?
-	
-	PeopleLocation(int ID ) {
+public class PeopleLocation { // this is a separate class so don't have to access thread
+
+	private final int ID; // each person has an ID
+	private Color myColor; // colour of the person
+	private AtomicBoolean inRoom; // are they in the club?
+	private AtomicBoolean arrived; // have they arrived at the club?
+	private GridBlock location; // which GridBlock are they on?
+
+	PeopleLocation(int ID) {
 		Random rand = new Random();
-		float c = rand.nextFloat(); //bit of a hack to get different colours
-		myColor = new Color(c, rand.nextFloat(), c);	//only set at beginning	by thread
-		inRoom = new AtomicBoolean(false); //not in club
-		arrived = new AtomicBoolean(false); //have not arrive outside
-		this.ID=ID;
+		float c = rand.nextFloat(); // bit of a hack to get different colours
+		myColor = new Color(c, rand.nextFloat(), c); // only set at beginning by thread
+		inRoom = new AtomicBoolean(false); // not in club
+		arrived = new AtomicBoolean(false); // have not arrive outside
+		this.ID = ID;
 	}
-	
-	//setter
-	public  void setInRoom(boolean in) {
+
+	// setter
+	public void setInRoom(boolean in) {
 		this.inRoom.set(in);
 	}
-	
-	//getter and setter
+
+	// getter and setter
 	public boolean getArrived() {
 		return arrived.get();
 	}
+
 	public void setArrived() {
-		this.arrived.set(true);;
+		this.arrived.set(true);
+		;
 	}
 
-//getter and setter
+	// getter and setter
 	public GridBlock getLocation() {
 		return location;
 	}
-	public  void setLocation(GridBlock location) {
+
+	public void setLocation(GridBlock location) {
 		this.location = location;
 	}
 
-	//getter
-	public  int getX() { return location.getX();}	
-	
-	//getter
-	public  int getY() {	return location.getY();	}
-	
-	//getter
-	public  int getID() {	return ID;	}
+	// getter
+	public int getX() {
+		return location.getX();
+	}
 
-	//getter
-	public  synchronized boolean inRoom() {
+	// getter
+	public int getY() {
+		return location.getY();
+	}
+
+	// getter
+	public int getID() {
+		return ID;
+	}
+
+	// getter
+	public synchronized boolean inRoom() {
 		return inRoom.get();
 	}
-	//getter and setter
-	public synchronized  Color getColor() { return myColor; }
-	public synchronized  void setColor(Color myColor) { this.myColor= myColor; }
+
+	// getter and setter
+	public synchronized Color getColor() {
+		return myColor;
+	}
+
+	public synchronized void setColor(Color myColor) {
+		this.myColor = myColor;
+	}
 }
